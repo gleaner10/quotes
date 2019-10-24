7d4b544f028db85d5f8fed4836ddad76e861300a
gleaner10 <56981720+gleaner10@users.noreply.github.com>
2019-10-25 03:16:52 +0530

    Update items.py

--- quotes/items.py
+++ quotes/items.py
@@ -7,3 +7,13 @@ import selinium
 class QuotesItem(scrapy.Item):
     # name = scrapy.Field()
     pass
+
+
+
+import scrapy
+import selinium
+
+
+class QuotesItem(scrapy.Item):
+    # name = scrapy.Field()
+    pass
